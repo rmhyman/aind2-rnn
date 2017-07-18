652c7f624f65a60a91e72035a7645ba97a759c7c
Ransford M Hyman Jr <ransford.hyman@gmail.com>
2017-07-18 07:37:46 -0700

    Completed Project files
    Uploading the completed projects from my AWS instance

--- my_answers.py
+++ my_answers.py
@@ -13,9 +13,13 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    for i in range(len(series) - window_size):
+        X.append(series[i:i+window_size])
+        y.append(series[i+window_size])
+
     # reshape each 
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    X.shape = (np.shape(X)[0:window_size])
     y = np.asarray(y)
     y.shape = (len(y),1)
 
@@ -23,12 +27,20 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5,input_shape=(window_size,1)))
+    model.add(Dense(1))
+ 
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    print("a:{},z:{}".format(ord('a'),ord('z')))
+    for i,c in enumerate(text):
+        if not c.isalpha() and c not in punctuation:
+            text = text.replace(str(c), " ")
 
     return text
 
@@ -38,9 +50,17 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for i in range(0,len(text) - window_size,step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
+
+
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200,input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars,activation='softmax'))
+    return model
